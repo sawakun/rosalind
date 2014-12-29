9e35461ff5360a118edb8b58f41f4466580526d6
Yohei Sawayama <yohei.sawayama@gmail.com>
2014-12-29 22:57:40 +0900

    gc (not solved)

---
+++ rosalind_gc.py
@@ -0,0 +1,35 @@
+#fin = open('hoge.txt', 'r')
+fin = open('rosalind_gc.txt', 'r')
+d = {}
+key = ''
+for line in fin:
+        if key != '':
+                if d.has_key(key):
+                        d[key] = d[key] + line.rstrip()
+                else:
+                        d[key] = line.rstrip()
+        if line[0] == '>':
+                key = line[1:].rstrip()
+
+gc = {}
+for key, value in d.items():
+        count = 0
+        for i in range(len(value)):
+                if value[i] == 'G' or value[i] == 'C':
+                        count = count + 1
+        gc[key] = float(count) / len(value) * 100.0
+
+for key, value in gc.items():
+        print key, value
+
+
+max_value = 0.0
+max_key = ''
+for key, value in gc.items():
+        if value > max_value:
+                max_value = value
+                max_key = key
+
+print max_key
+print('%.6f' % gc[max_key])
+
